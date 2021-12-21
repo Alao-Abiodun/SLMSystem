33bff541f33d474914037346e65128daba772c8d
Alao-Abiodun <46402042+Alao-Abiodun@users.noreply.github.com>
2021-12-22 00:29:44 +0100

    feat/endpoint for creation of new users

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
 
 dotenv.config();
 
@@ -13,8 +15,17 @@ const userRoute = require("./routes/user.route");
 const { PORT, DB_USERNAME, DB_PASSWORD } = process.env;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(cors());
+app.use(express.static("./public"));
+app.use(
+  fileUpload({
+    createParentPath: true,
+    limits: { fileSize: 2000000 },
+    abortOnLimit: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.status(200).json("The main application already started");
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,9 +5,18 @@ const { passwordHash } = require("../libs/bcrypt");
 
 exports.userSignUp = async (req, res) => {
   try {
-    const user = req.body;
-    user.password = await passwordHash(user.password);
-    const newUser = User.create(user);
+    const { firstname, lastname, email, password } = req.body;
+    const { image } = req.files;
+    image.mv("./public/uploads/images/" + "" + image.name);
+    const hashedPassword = await passwordHash(password);
+    const newUser = new User({
+      firstname,
+      lastname,
+      email,
+      password: hashedPassword,
+      image: image.name,
+    });
+    await newUser.save();
     return Response(res).success({ data: newUser }, 201);
   } catch (error) {
     return Response(res).error(error.message, 500);
